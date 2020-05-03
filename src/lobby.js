83497ef0089d17d32734bcad448e440a048257f4
Mart <themcman@gmail.com>
2020-05-03 14:31:39 -0500

    removed some uneccessary code

--- src/lobby.js
+++ src/lobby.js
@@ -1,30 +1,11 @@
 import React, { useState, useEffect } from "react";
 import firebase from "./firebase";
-import { useGameObject } from "./gameObject";
 import MessagesContent from "./messages/messagesContent";
 
 import "./lobby.scss";
 
 const Lobby = props => {
-  // // const [gameId] = useGameObject();
-  //   let [gameId, setGameId] = useState("0");
-  //   useEffect(() => {
-  //     firebase
-  //       .firestore()
-  //       .collection("games")
-  //       .onSnapshot(snapshot => {
-  //         // const currentGame = Math.max(...snapshot.docs.map(doc => parseInt(doc.id)));
-  //         console.log("docs", snapshot.docs);
-  //         const currentGame = snapshot.docs
-  //           .map(doc => doc.id)
-  //           .sort()
-  //           .reverse()[0];
-  //         setGameId(currentGame);
-  //       });
-  //   }, [gameId]);
-
   let [userList, setUserList] = useState([]);
-  let gameId = useGameObject();
 
   useEffect(() => {
     firebase
