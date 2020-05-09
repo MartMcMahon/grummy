574830e76618290abf2e7e3607626b134bccabe8
Mart <themcman@gmail.com>
2020-05-09 13:46:54 -0500

    localhost api_root

--- src/api.js
+++
@@ -1,7 +0,0 @@
-let api_root;
-if (process.env.NODE_ENV === "production") {
-  api_root = "https://grummy.mart.pizza:6969";
-} else {
-  api_root = "http://localhost:6969";
-}
-export default api_root;
