ffc8202de4c8c821952d1ae159cc92ac287e7942
Mart <themcman@gmail.com>
2020-05-13 00:48:04 -0500

    moved auth to App.js

--- src/App.js
+++ src/App.js
@@ -2,27 +2,31 @@ import React, { useEffect, useState } from "react";
 import CardArea from "./cardArea";
 import firebase from "./firebase";
 import MessageArea from "./messageArea";
-import { useAuth } from "./useAuth";
 import Lobby from "./lobby";
 
 import "./main.scss";
 
 function App() {
-
-  let user = useAuth(firebase);
+  let [user, setUser] = useState({uid: null});
   let [route, setRoute] = useState("/");
 
   useEffect(() => {
-    firebase.auth().signInAnonymously();
-  }, [user]);
+    const unsubscribe = firebase.auth().onAuthStateChanged(user => {
+      setUser(user);
+    });
+    return () => unsubscribe;
+  }, []);
 
-  console.log('user', user);
+  useEffect(() => {
+    firebase.auth().signInAnonymously();
+  }, []);
+
   const getRoute = route => {
     switch (route) {
       case "/":
         return <Lobby userId={user.uid || null} setRoute={setRoute} />;
       case "game":
-        return <CardArea userId={(user.uid||null)} setRoute={setRoute} />;
+        return <CardArea userId={user.uid || null} setRoute={setRoute} />;
       default:
         return <div>ya fucked up.</div>;
     }
