ee80d86b6d2ebed1f300024fa5caf51cb435db26
Mart <themcman@gmail.com>
2020-05-13 04:15:16 -0500

    new cardArea changes

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
 
   useEffect(() => {
     firebase.auth().signInAnonymously();
-  }, []);
+  }, [user]);
 
   const getRoute = route => {
     switch (route) {
--- src/cardArea.js
+++ src/cardArea.js
@@ -1,26 +1,115 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Card } from "./cards";
+import { useGameObject } from "./gameObject";
 import "./cardArea.scss";
 
+let api_root = "https://grummy.mart.pizza:8080";
+if (process.env.NODE_ENV === "development") {
+  api_root = "http://localhost:6969";
+}
+
 const CardArea = props => {
-  // let gameObject = useGameObject();
+  let [gameId, setGameId] = useState("0");
+  let [chair, setChair] = useState(0);
+  let [hand, setHand] = useState([]);
+  let [table, setTable] = useState([[], [], [], []]);
+  let [selected, setSelected] = useState([]);
+  let [selectedDiscard, setSelectedDiscard] = useState(-1);
+  let userId = props.userId;
+
+  // actions
+  const drawAction = event => {
+    console.log('draw');
+    axios.get(`${api_root}/draw/?userId=${userId}`).then(res => {
+      const newHand = res.data.hand.map(base_card => new Card(base_card));
+      setHand(newHand);
+    });
+  }
+
+  // effects
+  useEffect(() => {
+    axios
+      .get(`${api_root}/game_status`)
+      .then(res => {
+        setGameId(res.data.id);
+      })
+      .then(() => {
+        axios.put(`${api_root}/register_player?userId=${userId}`).then(res => {
+          setChair(res.data.chair);
+        });
+      });
+  }, [gameId, userId]);
+
+  useEffect(() => {
+    const game_pinger = setInterval(() => {
+      axios.get(`${api_root}/state?userId=${userId}`).then(res => {
+        const new_table = res.data.table.map((played_cards, i) =>
+          res.data.table[(i + chair) % 4].map(card => new Card(card))
+        );
+        setTable(new_table);
+        setHand(res.data.hand.map(card => new Card(card)));
+        // setDiscard(res.data.discard.map(card => new Card(card)));
+        // setTurn(res.data.turn);
+        // setPhase(res.data.phase);
+      });
+    }, 2000);
+    return () => {
+      clearInterval(game_pinger);
+    };
+  }, [chair, userId]);
 
   return (
     <div className="card-area">
       <div className="top-area">
-        <div className="opponent top-opponent">top opponent</div>
-        <div className="opponent left-opponent">left opponent</div>
-        <div className="opponent right-opponent">right opponent</div>
+        <div className="opponent top-opponent">
+          {table[1].map(card => {
+            return card.render();
+          })}
+          top opponent
+        </div>
+        <div className="opponent left-opponent">
+          {table[2].map(card => {
+            return card.render();
+          })}
+          left opponent
+        </div>
+        <div className="opponent right-opponent">
+          {table[3].map(card => {
+            return card.render();
+          })}
+          right opponent
+        </div>
       </div>
       <div className="draw-discard-area">
-        <div className="card-button deck">draw</div>
+        <div className="card-button deck" onClick={drawAction}>
+          draw
+        </div>
         <div className="card-button discard">discard</div>
         <div className="button card-button draw-from-pile-button">pickup</div>
         <div className="button card-button play-card-button">play</div>
         <div className="button card-button discard-card-button">discard</div>
       </div>
       <div className="player play-area">your cards</div>
-      <div className="player hand">your hand</div>
+      <div className="player hand">
+        your hand
+        {hand.map((card, i) => {
+          return card.render({
+            onClick: e => {
+                setSelectedDiscard(-1);
+                const index = selected.indexOf(i);
+                if (index === -1) {
+                  setSelected([...selected, i]);
+                } else {
+                  const newSelected = [...selected];
+                  newSelected.splice(index, 1);
+                  setSelected(newSelected);
+                }
+              }
+            }, {isSelected: selected.includes(i) }
+          );
+        })}
+      </div>
     </div>
   );
 };
