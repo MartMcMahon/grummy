f843fe1fc6ce500c5e4da49cd894d654cc5c6886
Mart <themcman@gmail.com>
2020-05-14 02:56:04 -0500

    redesign changes

--- src/cardArea.js
+++ src/cardArea.js
@@ -14,18 +14,49 @@ const CardArea = props => {
   let [chair, setChair] = useState(0);
   let [hand, setHand] = useState([]);
   let [table, setTable] = useState([[], [], [], []]);
+  let [discard, setDiscard] = useState([]);
   let [selected, setSelected] = useState([]);
   let [selectedDiscard, setSelectedDiscard] = useState(-1);
   let userId = props.userId;
 
   // actions
   const drawAction = event => {
-    console.log('draw');
+    console.log("draw");
     axios.get(`${api_root}/draw/?userId=${userId}`).then(res => {
       const newHand = res.data.hand.map(base_card => new Card(base_card));
       setHand(newHand);
     });
-  }
+  };
+
+  const playAction = event => {
+    if (selected.length > 0) {
+      axios
+        .put(
+          `${api_root}/play_cards?userId=${userId}&cards=${JSON.stringify(
+            selected
+          )}`
+        )
+        .then(res => {
+          console.log("play res", res);
+        });
+      setSelected([]);
+    }
+  };
+
+  const discardAction = event => {
+    if (selected.length === 1) {
+      axios
+        .put(`${api_root}/discard?userId=${userId}&index=${selected[0]}`)
+        .then(res => {
+          console.log("discard res", res);
+          setSelected([]);
+        });
+    } else if (selected.length > 1) {
+      alert("that's too many");
+    } else {
+      return true;
+    }
+  };
 
   // effects
   useEffect(() => {
@@ -49,7 +80,7 @@ const CardArea = props => {
         );
         setTable(new_table);
         setHand(res.data.hand.map(card => new Card(card)));
-        // setDiscard(res.data.discard.map(card => new Card(card)));
+        setDiscard(res.data.discard.map(card => new Card(card)));
         // setTurn(res.data.turn);
         // setPhase(res.data.phase);
       });
@@ -85,17 +116,58 @@ const CardArea = props => {
         <div className="card-button deck" onClick={drawAction}>
           draw
         </div>
-        <div className="card-button discard">discard</div>
-        <div className="button card-button draw-from-pile-button">pickup</div>
-        <div className="button card-button play-card-button">play</div>
-        <div className="button card-button discard-card-button">discard</div>
+        <div className="card-button discard">
+          {discard.map((card, i) => {
+            return (
+              <div
+                className={`discard-card${
+                  i === selectedDiscard ? " selected" : ""
+                }`}
+                onClick={e => {
+                  setSelected([]);
+                  if (i === selectedDiscard) {
+                    setSelectedDiscard(-1);
+                  } else {
+                    setSelectedDiscard(i);
+                  }
+                }}
+              >
+                {card.render()}
+              </div>
+            );
+          })}
+        </div>
+          {selectedDiscard > -1 && (
+          <div className="button card-button draw-from-pile-button">pickup</div>
+        )}
+        {selected.length > 0 && (
+          <div
+            className="button card-button play-card-button"
+            onClick={playAction}
+          >
+            play
+          </div>
+        )}
+        {selected.length === 1 && (
+          <div
+            className="button card-button discard-card-button"
+            onClick={discardAction}
+          >
+            discard
+          </div>
+        )}
       </div>
-      <div className="player play-area">your cards</div>
+      <div className="player play-area">
+        {table[0].map(card => {
+          return card.render();
+        })}
+      </div>
       <div className="player hand">
         your hand
         {hand.map((card, i) => {
-          return card.render({
-            onClick: e => {
+          return card.render(
+            {
+              onClick: e => {
                 setSelectedDiscard(-1);
                 const index = selected.indexOf(i);
                 if (index === -1) {
@@ -106,7 +178,8 @@ const CardArea = props => {
                   setSelected(newSelected);
                 }
               }
-            }, {isSelected: selected.includes(i) }
+            },
+            { isSelected: selected.includes(i) }
           );
         })}
       </div>
