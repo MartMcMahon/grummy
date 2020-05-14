0df049dc0831d1a1c1954b5129473f577b04bbf1
Mart <themcman@gmail.com>
2020-05-14 03:28:37 -0500

    removed warning

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import CardArea from "./cardArea";
 import firebase from "./firebase";
-import MessageArea from "./messageArea";
 import Lobby from "./lobby";
 
 import "./main.scss";
